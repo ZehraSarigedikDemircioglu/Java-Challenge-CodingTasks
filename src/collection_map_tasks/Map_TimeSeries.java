7f91d6e7cd2c3487d9179dd1ed450830924bef4e
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-24 13:20:05 -0400

    added coding tasks

--- src/collection_map_tasks/Map_TimeSeries.java
+++ src/collection_map_tasks/Map_TimeSeries.java
@@ -1,19 +1,47 @@
 package collection_map_tasks;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Map_TimeSeries {
 
     public static void main(String[] args) {
 
+        Map<Integer, Double> map1 = new HashMap<>();
+        map1.put(1, 1.0);
+        map1.put(2, 1.5);
+        map1.put(3, 2.0);
+
+        Map<Integer, Double> map2 = new HashMap<>();
+        map2.put(2, 1.0);
+        map2.put(3, 2.5);
+        map2.put(5, 1.0);
+
+        System.out.println(timeSeriesSum(map1, map2));
+
 
     }
 
-    public static Map<Integer, Double> timeSeriesSum(Map<Integer, Double> map) {
+    public static Map<Integer, Double> timeSeriesSum(Map<Integer, Double> map1, Map<Integer, Double> map2) {
 
+        Map<Integer, Double> map = new HashMap<>();
+        map.putAll(map1);
+        Set<Integer> keys = new HashSet<>();
+        keys.addAll(map1.keySet());
+        keys.addAll(map2.keySet());
+
+        boolean flag = true;
+        while (flag) {
+            map2.keySet().forEach((k) -> {
+                if (map.keySet().contains(k)) {
+                    map.put(k, map.get(k) + map2.get(k));
+                } else {
+                    map.put(k, map2.get(k));
+                }
+            });
+
+            if (keys.size() == map.keySet().size())
+                flag = false;
+        }
         return map;
     }
 }
