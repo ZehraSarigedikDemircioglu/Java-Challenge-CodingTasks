f477b8b2334e1b208b7992c284e0e0027e71ef2f
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-24 13:20:12 -0400

    added coding tasks

--- src/collection_map_tasks/Set_Intersection.java
+++ src/collection_map_tasks/Set_Intersection.java
@@ -1,4 +1,25 @@
 package collection_map_tasks;
 
+import java.util.Set;
+
 public class Set_Intersection {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static Set<Object> getIntersection(Set<Object> a, Set<Object> b) {
+
+        if (a.size() > b.size()) {
+            a.retainAll(b);
+            return a;
+        } else {
+            b.retainAll(a);
+            return b;
+        }
+    }
 }
+/*
+return a Set equal to the intersection of the parameter Sets
+*/
