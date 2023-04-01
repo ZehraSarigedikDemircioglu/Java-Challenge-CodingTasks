7f922672a41a1808142207779ac934b81e52ecf4
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-31 23:49:00 -0400

    added coding tasks

--- src/string_tasks/String_CountOfRepeatedChars.java
+++ src/string_tasks/String_CountOfRepeatedChars.java
@@ -2,3 +2,7 @@ package string_tasks;
 
 public class String_CountOfRepeatedChars {
 }
+/*
+        Write a method that compresses a string based on the count of repeated characters. aabcccddaaaa -->  a2b1c3d2a4
+     */
+
