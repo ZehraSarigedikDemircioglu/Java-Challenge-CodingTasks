2121ef0e82842d1aa9e0db1856d24753765d0687
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-31 23:48:22 -0400

    added coding tasks

---
+++ src/string_tasks/String_CountPossibleDistinctStrings.java
@@ -0,0 +1,21 @@
+package string_tasks;
+
+public class String_CountPossibleDistinctStrings {
+}
+ /*
+    Count of substrings having all distinct characters
+    Given a string str consisting of lowercase alphabets, the task is to find the number of possible substrings (not necessarily distinct) that consists of distinct characters only.
+        Examples:
+            Input: Str = “gffg”
+            Output: 6
+    Explanation:
+        All possible substrings from the given string are,
+        ( “g“, “gf“, “gff”, “gffg”, “f“, “ff”, “ffg”, “f“, “fg“, “g” )
+        Among them, the highlighted ones ( “g“, “gf“, “f“, “f“, “fg“, “g” ) consists of distinct characters only.
+            Input: str = “gfg”
+            Output: 5
+    Explanation:
+        All possible substrings from the given string are,
+        ( “g“, “gf“, “gfg”, “f“, “fg“, “g” )
+        Among them, the highlighted ( “g“, “gf“, “f“, “fg“, “g” ) consists of distinct characters only.
+     */
