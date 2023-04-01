060cb042efc6314426fec8ba40b2430efa4fb17b
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-31 23:48:32 -0400

    added coding tasks

--- src/string_tasks/String_Anagram.java
+++ src/string_tasks/String_Anagram.java
@@ -2,3 +2,8 @@ package string_tasks;
 
 public class String_Anagram {
 }
+/*
+    Write a  function that check if a string is build out of the same letters as another string.
+            Ex: same("abc", "cab"); -> true , Solution 1:
+                same("abc", "abb"); -> false:
+     */
