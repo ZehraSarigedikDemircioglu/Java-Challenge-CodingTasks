0de3fd22daa0c4719c7903e89f21bb172bdc4199
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-31 23:50:33 -0400

    added coding tasks

--- src/string_tasks/String_ConvertStringToInteger.java
+++ src/string_tasks/String_ConvertStringToInteger.java
@@ -3,5 +3,5 @@ package string_tasks;
 public class String_ConvertStringToInteger {
 }
 /*
-    write a program that can convert a string to integer. DO NOT use parse or valueOf methods
-     */
+write a program that can convert a string to integer. DO NOT use parse or valueOf methods
+*/
